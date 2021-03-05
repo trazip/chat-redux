a39c68a058ecf9b69d962058d4e581de302238ab
Pierre Penhard <p.penhard@gmail.com>
2021-03-05 14:18:11 +0100

    Added redux-logger and redux-promise

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
+import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
@@ -13,9 +15,18 @@ const reducers = combineReducers({
   changeMe: (state = null, action) => state
 });
 
+const middlewares = applyMiddleware(logger, reduxPromise);
+
+const initialState = {
+  messages: [],
+  channels: [ 'general', 'react', 'paris' ],
+  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
+  selectedChannel: 'general'
+};
+
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, initialState, middlewares)}>
     <App />
   </Provider>,
   document.getElementById('root')
